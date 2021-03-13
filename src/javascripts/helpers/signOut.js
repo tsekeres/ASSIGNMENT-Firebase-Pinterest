d3c0a849a6c0c27ad621811d59ce0d04a086031c
tsekeres <twsekeres@gmail.com>
2021-03-13 10:25:21 -0600

    fix for duplicate forms

--- src/javascripts/helpers/signOut.js
+++ src/javascripts/helpers/signOut.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const signOut = () => {
+  document.querySelector("body").removeEventListener("click", domClickEvents);
   firebase.auth().signOut();
 };
 
