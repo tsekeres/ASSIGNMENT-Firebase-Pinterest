c5b5abc2e0d7bc5b68ab31901f6ee5b0017c8e42
tsekeres <twsekeres@gmail.com>
2021-03-13 09:28:32 -0600

    small fixes

--- src/javascripts/components/user.js
+++ src/javascripts/components/user.js
@@ -1,6 +1,6 @@
 const showUser = (user) => {
   document.querySelector('#user-title').innerHTML = `
-  <div id="user-area">
+  <div id="user-data">
     <img id="user-image" src="${user.photoURL}">
     <h3 id="user-name">${user.displayName}</h3>
   </div>`;
